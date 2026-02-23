7be09be9e560168970591c0ed5ffb0b0752566a1
hargrovn4758 <hargrovn4758@student.faytechcc.edu>
2026-02-22 20:56:08 -0500

    Add files via upload

---
+++ P1LAB1_HargroveNicole.py
@@ -0,0 +1,13 @@
+# Nicole Hargrove
+# February 20, 2026
+# P1LAB1
+# This program asks the user for their first and last name and displays a welcome message.
+
+# Ask user for first name
+first_name = input("Enter your first name: ")
+
+# Ask user for last name
+last_name = input("Enter your last name: ")
+
+# Display welcome message
+print("Hello,", first_name, last_name + "! Welcome to CTI-110")
